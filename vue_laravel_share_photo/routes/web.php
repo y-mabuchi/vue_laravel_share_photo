7f5f5141bfe54d4b0c20ba92e8ec824096bd854b
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-17 06:29:25 +0900

    エラーが出るのでアロー関数を停止

--- vue_laravel_share_photo/routes/web.php
+++ vue_laravel_share_photo/routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+Route::get('/{any?}', function () {
+    return view('index');
+})->where('any', '.+');
