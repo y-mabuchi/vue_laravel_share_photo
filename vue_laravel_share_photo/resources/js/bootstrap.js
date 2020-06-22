eb73044bc7bb71368bd1e2c3caac8e439f656c78
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-23 06:44:05 +0900

    ログイン、ログアウト処理を追加

--- vue_laravel_share_photo/resources/js/bootstrap.js
+++ vue_laravel_share_photo/resources/js/bootstrap.js
@@ -1,3 +1,4 @@
+import { getCookieValue } from './util'
 window._ = require('lodash');
 
 /**
@@ -11,7 +12,7 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -22,6 +23,12 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.request.use(config => {
+    config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+
+    return config
+})
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
---
+++ vue_laravel_share_photo/resources/js/util.js
@@ -0,0 +1,22 @@
+/**
+ * クッキーの値を取得する
+ *
+ * @param   {String}  searchKey  検索するキー
+ * @return  {String}             キーに対する値
+ */
+export function getCookieValue(searchKey) {
+  if (typeof searchKey === 'undefined') {
+    return ''
+  }
+
+  let val = ''
+
+  document.cookie.split(';').forEach(cookie => {
+    const [key, value] = cookie.split('=')
+    if (key === searchKey) {
+      return val = value
+    }
+  })
+
+  return val
+}
